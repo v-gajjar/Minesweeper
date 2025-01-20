fa282e99134422397fe7bb815716623cc6332d16
vgajjar <vgajjar22@gmail.com>
2025-01-20 17:16:13 +0000

    move GameDifficultyLevel to enum folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import GameDifficultyLevel from './GameDifficultyLevel';
+import GameDifficultyLevel from './enum/GameDifficultyLevel';
 import './App.css'
 
 function App() {
