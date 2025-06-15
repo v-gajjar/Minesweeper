b46cd6466470739537d0a0427b1a5a9ddcecf492
vgajjar <vgajjar22@gmail.com>
2025-06-15 17:55:44 +0100

    add config for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Minesweeper/",
   test: {
     environment: 'jsdom',
     globals: true,  // <-- THIS LINE is the key
