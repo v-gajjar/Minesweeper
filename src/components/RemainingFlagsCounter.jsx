d9c92bf93642096355905ac819e148f692c696b5
vgajjar <vgajjar22@gmail.com>
2025-07-17 12:19:14 +0100

    fix various issues - spacing, alignment, and add wrap the number count in a span with a fixed width to stop the label and flag icon from jumping when the numbers change.

--- src/components/RemainingFlagsCounter.jsx
+++ src/components/RemainingFlagsCounter.jsx
@@ -7,9 +7,10 @@ function RemainingFlagsCounter({ remainingFlagsCount }) {
         <div id="remainingFlagsCounter" data-testid="flags-remaining">
 
             <AccessibleIcon label="flag icon">
-                <Flag size={25} color="#c01c28" weight="fill" style={{ margin: "0px 10px 0px 0px" }}/>
+                <Flag size={25} color="#c01c28" weight="fill" />
             </AccessibleIcon>
-            Remaining Flags: {remainingFlagsCount}
+            <span id="remainingFlagsLabel">Remaining Flags:</span>
+            <span id="remainingFlagsCount">{remainingFlagsCount}</span>
 
         </div>
     );
