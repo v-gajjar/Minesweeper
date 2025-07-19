193a835e3e4991b3e44f2bf9ffc87b7fe4efb26b
Heliomar Peña <penaheliomar@gmail.com>
2025-07-19 17:29:25 -0300

    fix: avoid re-render in remaining flags counter and game difficulty selector

--- src/components/GameDifficultySelector.jsx
+++ src/components/GameDifficultySelector.jsx
@@ -1,26 +1,25 @@
 import { GAME_DIFFICULTY_LEVEL_SETTINGS } from "../config/gameDifficultyLevelSettings";
+import { memo } from "react";
 
-function GameDifficultySelector({gameDifficultySettings, onChange}) {
-
-    return (
-        <div className='game-difficulty-select-wrapper'>
-        <label>Difficulty: </label>
-        <select 
-            value={gameDifficultySettings.level}
-            onChange={onChange}
-            name="game-difficulty-select" 
-            id="game-difficulty-select"
-            data-testid="difficulty-select"
-        > 
+function GameDifficultySelector({ gameDifficultySettings, onChange }) {
+  return (
+    <div className="game-difficulty-select-wrapper">
+      <label>Difficulty: </label>
+      <select
+        value={gameDifficultySettings.level}
+        onChange={onChange}
+        name="game-difficulty-select"
+        id="game-difficulty-select"
+        data-testid="difficulty-select"
+      >
         {Object.keys(GAME_DIFFICULTY_LEVEL_SETTINGS).map((setting, i) => (
-            <option 
-                key={i}
-                value={GAME_DIFFICULTY_LEVEL_SETTINGS[setting].level}
-            >{GAME_DIFFICULTY_LEVEL_SETTINGS[setting].label}</option>
+          <option key={i} value={GAME_DIFFICULTY_LEVEL_SETTINGS[setting].level}>
+            {GAME_DIFFICULTY_LEVEL_SETTINGS[setting].label}
+          </option>
         ))}
       </select>
     </div>
-    );
+  );
 }
 
-export default GameDifficultySelector;
+export default memo(GameDifficultySelector);
