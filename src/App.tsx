eb373e5be3813643ee02b6f4b8522ee93e24d899
Heliomar Peña <penaheliomar@gmail.com>
2025-07-21 10:20:32 -0300

    fix: removed .jsx from renamed files' imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect, useRef } from "react";
 
 import { GAME_DIFFICULTY_LEVEL_SETTINGS } from "./config/gameDifficultyLevelSettings.js";
 
-import GameDifficultySelector from "./components/GameDifficultySelector.jsx";
-import GameBoard from "./components/GameBoard.jsx";
-import GameResultModal from "./components/GameResultModal.jsx";
-import RemainingFlagsCounter from "./components/RemainingFlagsCounter.jsx";
+import GameDifficultySelector from "./components/GameDifficultySelector";
+import GameBoard from "./components/GameBoard";
+import GameResultModal from "./components/GameResultModal";
+import RemainingFlagsCounter from "./components/RemainingFlagsCounter";
 
 import { 
   getMineLocations,
