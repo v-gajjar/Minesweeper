277a4916412eb9d898d42a16f69d38eafb24a8be
Engombe Lokanga <engombeel13@gmail.com>
2025-09-25 12:34:34 +0100

    Updated vite config & tsconfig to resolve case-sensitivity component issue between Windows & Linux

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/Minesweeper/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@features': path.resolve(__dirname, './src/components/feature'),
+      '@config': path.resolve(__dirname, './src/config'),
+      '@enum': path.resolve(__dirname, './src/enum'),
+      '@types': path.resolve(__dirname, './src/types'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
