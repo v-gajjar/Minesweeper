53b00a09179c545cdc0084fe8fdd6b560647f7af
Shawn Niedetzki <shawn@niedetzki.com>
2025-10-07 15:25:04 -0400

    Fixed negative flags possible

--- src/App.tsx
+++ src/App.tsx
@@ -182,6 +182,10 @@ function App() {
   const onToggleFlag = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
 
+    if (remainingFlagsCount === 0 || gameStatus !== GameStatus.GAME_IN_PROGRESS) {
+      return;
+    }
+
     const target = event.currentTarget as unknown as {
       dataset: LocationColRow;
     };
