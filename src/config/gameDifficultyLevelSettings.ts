720376ffe1f686f0eed82bfc1f10fceabfe4cd6c
vgajjar <vgajjar22@gmail.com>
2025-10-31 15:47:16 +0000

    refactoor the difficutly config to use the new types

--- src/config/gameDifficultyLevelSettings.ts
+++ src/config/gameDifficultyLevelSettings.ts
@@ -1,31 +1,24 @@
-import GameDifficultyLevel from '../enum/GameDifficultyLevel';
-import type { GameDifficultyLevelSettings } from './gameDifficultyLevelSettings.interfaces';
+import type { DifficultyConfig, DifficultyLevel } from '@/types';
 
-export const GAME_DIFFICULTY_LEVEL_SETTINGS: GameDifficultyLevelSettings = {
+export const GAME_DIFFICULTY_LEVEL_SETTINGS: Record<
+  DifficultyLevel,
+  DifficultyConfig
+> = {
   EASY: {
-    level: GameDifficultyLevel.EASY,
-    boardSize: {
-      rowCount: 9,
-      columnCount: 9,
-    },
+    level: 'EASY',
+    boardSize: { rowCount: 9, columnCount: 9 },
     mineCount: 10,
     label: 'Beginner',
   },
   MEDIUM: {
-    level: GameDifficultyLevel.MEDIUM,
-    boardSize: {
-      rowCount: 16,
-      columnCount: 16,
-    },
+    level: 'MEDIUM',
+    boardSize: { rowCount: 16, columnCount: 16 },
     mineCount: 40,
     label: 'Intermediate',
   },
   HARD: {
-    level: GameDifficultyLevel.HARD,
-    boardSize: {
-      rowCount: 16,
-      columnCount: 30,
-    },
+    level: 'HARD',
+    boardSize: { rowCount: 16, columnCount: 30 },
     mineCount: 80,
     label: 'Expert',
   },
