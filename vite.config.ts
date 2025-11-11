960b3f2b0123acc9d7cdea8d71a9dfc9154284ef
vgajjar <vgajjar22@gmail.com>
2025-11-11 10:51:35 +0000

    add camelCase conversion back into vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   base: '/Minesweeper/',
   css: {
     modules: {
+      localsConvention: 'camelCase',
       generateScopedName: (name: string) => name, // keeps the literal name, no hash
     },
   },
