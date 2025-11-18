1daae9f2d275e8f1fdd0a326f8fbc2b22a5363ea
vgajjar <vgajjar22@gmail.com>
2025-11-18 12:06:49 +0000

    re add the shouldRender prop and animation related logic

--- src/components/feature/ResultModal/ResultModal.tsx
+++ src/components/feature/ResultModal/ResultModal.tsx
@@ -1,13 +1,35 @@
+import { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import type { ResultModalProps } from '@/components/feature/ResultModal/ResultModal.interface';
 import styles from '@components/feature/ResultModal/ResultModal.module.css';
 
 function ResultModal({ open = false, gameWon, onClick }: ResultModalProps) {
-  if (!open) return null;
+  const [shouldRender, setShouldRender] = useState(open);
 
   const message = gameWon ? 'You Won!' : 'Game Over!';
   const gameResultClass = gameWon ? styles.gameWonModal : styles.gameLostModal;
+
+  useEffect(() => {
+    if (open) {
+      setShouldRender(true);
+    }
+  }, [open]);
+
+  // When exit animation ends, unmount the modal
+  const handleAnimationEnd = () => {
+    if (!open) {
+      setShouldRender(false);
+    }
+  };
+
   const modalClass = classNames(styles.resultModal, gameResultClass);
+
+  if (!shouldRender) return null;
+
+  /*
+      IMPORTANT NOTE: 
+      The data-state attribute is used to trigger CSS animations for modal enter and exit - please do not remove it
+  */
 
   return (
     <div className={styles.modalOverlay} data-testid={`result-modal`}>
@@ -18,7 +40,7 @@ function ResultModal({ open = false, gameWon, onClick }: ResultModalProps) {
         role='dialog'
         aria-labelledby='game-result-message'
         aria-describedby='game-result-description'
-        onAnimationEnd={() => {}}
+        onAnimationEnd={handleAnimationEnd}
       >
         <p id='game-result-message' className={styles.resultModalMessage}>
           {message}
