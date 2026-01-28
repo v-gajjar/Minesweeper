69f1ac6e3f4b42a8957f4546755a1ed86d2bc57d
Hannah Olbrich <hannaholbrich@gmail.com>
2026-01-28 21:06:04 +0000

    Add in gameboard component test

--- tests/components/GameBoard2.test.jsx
+++ tests/components/GameBoard2.test.jsx
@@ -1,12 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import GameBoard from '../../components/GameBoard/GameBoard';
+import GameBoard from '../../components/GameBoard/GameBoard.jsx';
 
 describe('GameBoard Component', () => {
-  it('should render the GameBoard component', () => {
+  it('renders the GameBoard component', () => {
     render(<GameBoard />);
     const gameBoardElement = screen.getByTestId('game-board');
     expect(gameBoardElement).toBeInTheDocument();
   });
-
 });
