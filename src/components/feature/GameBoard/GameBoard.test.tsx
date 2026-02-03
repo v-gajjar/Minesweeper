3f5116d595cfd307c53829f765932470ab670c64
Hannah Olbrich <hannaholbrich@gmail.com>
2026-02-03 13:29:20 +0000

    Add gameboard component tests

--- src/components/feature/GameBoard/GameBoard.test.tsx
+++ src/components/feature/GameBoard/GameBoard.test.tsx
@@ -1,7 +1,29 @@
+import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
+import GameBoard from './GameBoard';
 
-describe('WIP', () => {
-  it.skip('add tests', () => {
-    expect(true).toBe(true);
-  });
+describe('GameBoard component', () => {
+  it('renders GameBoard and displays cells', () => {
+  const mockBoard = [
+    [
+      { isRevealed: false, hasMine: false, isFlagged: false, adjacentMinesCount: 0, x: 0, y: 0 },
+      { isRevealed: false, hasMine: true, isFlagged: true, adjacentMinesCount: 1, x: 0, y: 1 }
+    ],
+    [
+      { isRevealed: true, hasMine: false, isFlagged: false, adjacentMinesCount: 1, x: 1, y: 0 },
+      { isRevealed: false, hasMine: false, isFlagged: false, adjacentMinesCount: 2, x: 1, y: 1 }
+    ]
+  ];
+
+  render(
+    <GameBoard
+      boardSize={{ rowCount: 2, columnCount: 2 }}
+      board={mockBoard}
+      onClick={() => {}}
+      onContextMenu={() => {}}
+    />
+  );
+  
+  expect(screen.getAllByTestId('cell')).toHaveLength(4);
+});
 });
