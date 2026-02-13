1d7fdfe9224693ef569a9ef07ab779f90dfc6e31
Hannah Olbrich <hannaholbrich@gmail.com>
2026-02-13 13:07:24 +0000

    Add two component tests for cell.tsx

--- src/components/feature/GameBoard/Cell/Cell.test.tsx
+++ src/components/feature/GameBoard/Cell/Cell.test.tsx
@@ -1,7 +1,35 @@
 import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Cell from './Cell';
 
-describe('WIP', () => {
-  it.skip('add tests', () => {
-    expect(true).toBe(true);
+describe('cells', () => {
+  const defaultCell = {
+    x: 0,
+    y: 0,
+    hasMine: false,
+    isRevealed: false,
+    isFlagged: false,
+    hasExplodedMine: false,
+    isIncorrectlyFlagged: false,
+    adjacentMinesCount: 0,
+  };
+
+  it ('should render an incorrectly flagged cell', () => {
+    render(<Cell 
+      cell={{ ...defaultCell, isFlagged: true, isIncorrectlyFlagged: true }} 
+      onClick={() => {}}
+      onContextMenu={() => {}}
+    />);
+    expect(screen.getByTestId('x-icon')).toBeInTheDocument();
+  });
+
+  it('should render adjacent mines count when revealed', () => {
+    render(<Cell 
+      cell={{ ...defaultCell, isRevealed: true, adjacentMinesCount: 3 }} 
+      onClick={() => {}}
+      onContextMenu={() => {}}
+    />);
+    expect(screen.getByText('3')).toBeInTheDocument();
   });
 });
